8d25ef1d1c41db6db86239c24bbf6e12d22e0100
Maxwell Orok <morok@mevex.com>
2018-09-13 12:07:09 -0400

    init commit

---
+++ src/main.cpp
@@ -0,0 +1,13 @@
+// Loose spec from https://nextjournal.com/sdanisch/the-julia-challenge
+// Needs to work for n arguments
+// Needs to work for any combinations of scalars and n-dimensional arrays
+// Inside the implementation, one needs to have access to the whole call tree
+// and it should be possible to rewrite that tree or flatten it into one loop
+// User defined functions and types need to be inlined and SIMD acceleration
+// should be used whenever possible
+
+#include <omp.h> // ;)
+
+auto main() -> int {
+
+}
