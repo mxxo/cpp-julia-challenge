7d427614f9b29e7935eaf68e889e6c309aa90a9f
Maxwell Orok <morok@mevex.com>
2018-09-13 15:40:04 -0400

    add a user-defined function

--- src/main.cpp
+++ src/main.cpp
@@ -6,8 +6,22 @@
 // User defined functions and types need to be inlined and SIMD acceleration
 // should be used whenever possible
 
+// Code must not be much longer while still being readable.
+// Exemptions for languages with tiny standard libraries.
+// -> variadic templates are readable right ;)
+
 #include <omp.h> // ;)
+#include <iostream>
+
+// a simple user-defined function
+auto user_defined(auto&& x) {
+  return x + 1;
+}
 
 auto main() -> int {
-  
+  std::cout << user_defined(2) << std::endl;
+  std::cout << user_defined(3.5) << std::endl;
+  auto i = 1;
+  std::cout << user_defined(i) << std::endl;
+  std::cout << i << std::endl;
 }
